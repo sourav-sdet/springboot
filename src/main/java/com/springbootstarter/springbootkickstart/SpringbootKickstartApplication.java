f2ae2338aeb918ce235d6fae3b54b4de708c0916
Sourav <srvmishra.007@gmail.com>
2022-11-04 21:28:37 +0530

    First Springboot Commit

---
+++ src/main/java/com/springbootstarter/springbootkickstart/SpringbootKickstartApplication.java
@@ -0,0 +1,13 @@
+package com.springbootstarter.springbootkickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKickstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootstarter/springbootkickstart/SpringbootKickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootstarter.springbootkickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
